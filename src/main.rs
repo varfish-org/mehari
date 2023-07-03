57e7152efde0f593f23f9fd8f1e28c550ca984ce
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-07-03 17:13:18 +0200

    feat: mehari becomes available as a library

--- src/main.rs
+++ src/main.rs
@@ -72,33 +72,9 @@
 //! $ cargo add mehari
 //! ```
 
-#[cfg(feature = "documentation")]
-#[doc = include_str!("../docs/index.md")]
-pub mod user_doc {
-    #[doc = include_str!("../docs/getting_started.md")]
-    pub mod getting_started {}
-
-    #[doc = include_str!("../docs/anno_seqvars.md")]
-    pub mod anno_seqvars {}
-
-    #[doc = include_str!("../docs/anno_strucvars.md")]
-    pub mod anno_strucvars {}
-
-    #[doc = include_str!("../docs/db_build.md")]
-    pub mod db_build {}
-
-    #[doc = include_str!("../docs/implementation_notes.md")]
-    pub mod implementation_notes {}
-}
-
-pub mod annotate;
-pub mod common;
-pub mod db;
-pub mod ped;
-pub mod server;
-pub mod verify;
-
 use clap::{command, Args, Parser, Subcommand};
+
+use mehari::{annotate, common, db, server, verify};
 
 #[derive(Debug, Parser)]
 #[command(
