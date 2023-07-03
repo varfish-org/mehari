59bb41a7c4576e616a788b8788bab941fc6424ab
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-07-03 17:46:14 +0200

    feat: mehari becomes available as a library (#118)

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+//! Mehari library main entry point.
+
+#[cfg(feature = "documentation")]
+#[doc = include_str!("../docs/index.md")]
+pub mod user_doc {
+    #[doc = include_str!("../docs/getting_started.md")]
+    pub mod getting_started {}
+
+    #[doc = include_str!("../docs/anno_seqvars.md")]
+    pub mod anno_seqvars {}
+
+    #[doc = include_str!("../docs/anno_strucvars.md")]
+    pub mod anno_strucvars {}
+
+    #[doc = include_str!("../docs/db_build.md")]
+    pub mod db_build {}
+
+    #[doc = include_str!("../docs/implementation_notes.md")]
+    pub mod implementation_notes {}
+}
+
+pub mod annotate;
+pub mod common;
+pub mod db;
+pub mod ped;
+pub mod server;
+pub mod verify;
