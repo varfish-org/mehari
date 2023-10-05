85514cfeccd0ad77abb52b2510599381d15f35e1
Max Zhao <max.zhao@arsbrevis.de>
2023-10-05 09:18:19 +0200

    Adding failing test case

--- src/annotate/seqvars/mod.rs
+++ src/annotate/seqvars/mod.rs
@@ -1699,4 +1699,39 @@ mod test {
 
         Ok(())
     }
+
+    /// Mitochondnrial variants called by the DRAGEN v310 germline caller have special format
+    /// considerations.
+    ///
+    /// See: https://support-docs.illumina.com/SW/DRAGEN_v310/Content/SW/DRAGEN/MitochondrialCalling.htm
+    #[test]
+    fn test_dragen_mitochondrial_variant() -> Result<(), anyhow::Error> {
+        let temp = TempDir::default();
+        let path_out = temp.join("output.tsv");
+
+        let args_common = crate::common::Args {
+            verbose: Verbosity::new(0, 1),
+        };
+        let args = Args {
+            genome_release: None,
+            path_db: String::from("tests/data/annotate/db"),
+            path_input_vcf: String::from("tests/data/db/create/mitochondrial_variants.vcf"),
+            output: PathOutput {
+                path_output_vcf: None,
+                path_output_tsv: Some(path_out.into_os_string().into_string().unwrap()),
+            },
+            max_var_count: None,
+            path_input_ped: Some(String::from(
+                "tests/data/db/create/mitochondrial_variants.ped",
+            )),
+        };
+
+        run(&args_common, &args)?;
+
+        let actual = std::fs::read_to_string(args.output.path_output_tsv.unwrap())?;
+        let expected = std::fs::read_to_string("tests/data/db/create/badly_formed_vcf_entry.tsv")?;
+        assert_eq!(&expected, &actual);
+
+        Ok(())
+    }
 }
