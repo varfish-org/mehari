38a188632249d938d5146291251ad9570dcb7953
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-10-23 12:07:13 +0200

    wip

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 // The custom build script, needed as we use prost.
 
 fn main() {
+    println!("cargo:rerun-if-changed=src/db/create/txs/data.proto3");
     prost_build::compile_protos(&["src/db/create/txs/data.proto3"], &["src/"]).unwrap();
 }
