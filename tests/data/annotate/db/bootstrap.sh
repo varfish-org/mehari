5f90cdb3e609e0905f755052c462707a5d64d0e1
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-02-26 13:19:58 +0100

    wip

--- tests/data/annotate/db/bootstrap.sh
+++ tests/data/annotate/db/bootstrap.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e98786f63ed491948133bf19d83df2026daafc63c16d5be7517e0c5946736f4
-size 2569
+oid sha256:d9a18e2bd72aeabbbd0efa775983ba40651a73f377c32d349f0a21fcc26fb9cc
+size 2846
