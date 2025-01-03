f1c010fe8dd74330d1293c47153e38ba35b4cb78
Till Hartmann <till.hartmann@bih-charite.de>
2025-01-03 13:58:10 +0100

    update entrypoint to match new server run cli

--- utils/docker/entrypoint.sh
+++ utils/docker/entrypoint.sh
@@ -7,15 +7,22 @@ set -euo pipefail
 #
 #   PATH_DB         -- path to the database directory containing,
 #                      e.g., `grch3{7,8}/*.zst`.
-#                      default: /data/hpo
+#                      default: /data/mehari/db
 #   HTTP_HOST       -- host to listen on
 #                      default: 0.0.0.0
 #   HTTP_PORT       -- port
 #                      default: 8080
 
-PATH_DB=${PATH_DB-/data/mehari}
+PATH_DB=${PATH_DB-/data/mehari/db}
 HTTP_HOST=${HTTP_HOST-0.0.0.0}
 HTTP_PORT=${HTTP_PORT-8080}
+
+PATH_TRANSCRIPTS_37=$PATH_DB/grch37/seqvars/txs.bin.zst
+PATH_TRANSCRIPTS_38=$PATH_DB/grch38/seqvars/txs.bin.zst
+PATH_FREQUENCIES_37=$PATH_DB/grch37/seqvars/freqs
+PATH_FREQUENCIES_38=$PATH_DB/grch38/seqvars/freqs
+PATH_CLINVAR_37=$PATH_DB/grch37/seqvars/clinvar
+PATH_CLINVAR_38=$PATH_DB/grch38/seqvars/clinvar
 
 first=${1-}
 
@@ -26,7 +33,12 @@ else
   exec \
     mehari \
     server run \
-      --path-db "$PATH_DB" \
+      $(test -e "$PATH_TRANSCRIPTS_37" && echo --transcripts "$PATH_TRANSCRIPTS_37") \
+      $(test -e "$PATH_TRANSCRIPTS_38" && echo --transcripts "$PATH_TRANSCRIPTS_38") \
+      $(test -e "$PATH_FREQUENCIES_37" && echo --frequencies "$PATH_FREQUENCIES_37") \
+      $(test -e "$PATH_FREQUENCIES_38" && echo --frequencies "$PATH_FREQUENCIES_38") \
+      $(test -e "$PATH_CLINVAR_37" && echo --clinvar "$PATH_CLINVAR_37") \
+      $(test -e "$PATH_CLINVAR_38" && echo --clinvar "$PATH_CLINVAR_38") \
       --listen-host "$HTTP_HOST" \
       --listen-port "$HTTP_PORT"
 fi
